80b8080c251fa74eae31f529f919ce75abc55d99
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-05-08 23:49:15 +0100

    Updates

--- src/ListedItem.java
+++ src/ListedItem.java
@@ -1,2 +1,19 @@
-public class ListedItem {
+public abstract class ListItem {
+    // write code here
+
+    protected ListItem rightLink;
+    protected ListItem leftLink;
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+
+    public abstract void next();
+    public abstract void setNext();
+    public abstract void previous();
+    public abstract void setPrevious();
+    private void compareTo();
+
+
 }
